8e4782ba3c09f97257cb89f8d8feed5b917b86ab
symo <simon.hamanek@gmail.com>
2020-10-22 12:42:54 +0100

    test

--- Aernautica Imperials/Program.cs
+++ Aernautica Imperials/Program.cs
@@ -4,6 +4,7 @@ namespace Aernautica_Imperials {
     class Program {
         static void Main(string[] args) {
             Console.WriteLine("Hello World!");
+            Console.WriteLine();
         }
     }
 }
